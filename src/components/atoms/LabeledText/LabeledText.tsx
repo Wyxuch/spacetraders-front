f778aebc91caab4611d498b0966214b86c85b4df
jarek.kowalczyk <jarek.kowalczyk@dcx.bz>
2024-03-19 20:41:20 +0100

    add text component

---
+++ src/components/atoms/LabeledText/LabeledText.tsx
@@ -0,0 +1,17 @@
+import { Textarea } from '@components/shadcn/ui/textarea';
+
+interface Props {
+  label: string;
+  text: string | number;
+}
+
+export const LabeledText = ({ label, text }: Props) => {
+  return (
+    <div className={'flex m-2'}>
+      <p className={'pr-2 py-1 w-20 text-right'}>{label}:</p>
+      <Textarea value={text} disabled />
+    </div>
+  );
+};
+
+export default LabeledText;
