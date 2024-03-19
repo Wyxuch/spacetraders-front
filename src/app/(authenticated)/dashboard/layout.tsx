cfc91d4a72843696cf408ad07620d87d9f10c131
Krzysiek <krzyslenarcik@gmail.com>
2024-03-19 20:17:02 +0100

    add route

--- src/app/(authenticated)/dashboard/layout.tsx
+++ src/app/(authenticated)/dashboard/layout.tsx
@@ -9,13 +9,17 @@ import { Button } from '@components/shadcn/ui/button';
 import { Card, CardContent } from '@components/shadcn/ui/card';
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  const router = useRouter()
   const { removeToken } = useAuthContext();
 
   return (
     <div className={'flex p-4 min-h-screen gap-4'}>
       <nav>
         <Card className={'h-full w-40 p-2'}>
-          <Button variant={'outline'} className={'w-full text-left justify-between mb-2'}>
+          <Button onClick={() => router.push('dashboard/ship')} variant={'outline'} className={'w-full text-left justify-between mb-2'}>
+            Ship
+          </Button>
+          <Button onClick={() => router.push('dashboard/scan')} variant={'outline'} className={'w-full text-left justify-between mb-2'}>
             Scan
           </Button>
           <Button onClick={removeToken} variant={'destructive'} className={'w-full text-left justify-between'}>
