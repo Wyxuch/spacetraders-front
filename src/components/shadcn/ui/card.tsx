c947e0052893a6af212864b0e828a744b60d2d46
wyxuch <stefankieubasa@gmail.com>
2024-03-27 21:41:01 +0100

    feat: change card shadow

--- src/components/shadcn/ui/card.tsx
+++ src/components/shadcn/ui/card.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { cn } from 'src/utils/shadcn/utils';
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('rounded-lg border bg-card text-card-foreground shadow-sm', className)} {...props} />
+  <div ref={ref} className={cn('rounded-lg border bg-card text-card-foreground shadow-md shadow-violet-900/20', className)} {...props} />
 ));
 Card.displayName = 'Card';
 
