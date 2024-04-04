1dd59da40f7a0247b4b12992ea433c48de7bb8c3
Kikolaczek <krzyslenarcik@gmail.com>
2024-04-04 19:27:35 +0200

    accept contract

--- src/app/(authenticated)/dashboard/contracts/page.tsx
+++ src/app/(authenticated)/dashboard/contracts/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+
 import { BASE_URL } from '@consts/common';
+
 import { ContractData, ContractResponse, NoContractResponse } from '@api/types';
 import { useShipsContext } from '@context/ShipsContext';
-import { useApi } from '@hooks/useApi';
+
+import Paragraph from '@components/atoms/Typography/Paragraph';
 import { Button } from '@components/shadcn/ui/button';
 import { Card, CardContent } from '@components/shadcn/ui/card';
 import { Separator } from '@components/shadcn/ui/separator';
-import Paragraph from '@components/atoms/Typography/Paragraph';
+
+import { useApi } from '@hooks/useApi';
 
 export default function Home() {
   const fetch = useApi();
@@ -17,28 +21,27 @@ export default function Home() {
   const [nocontract, setNoContract] = useState<ContractData | undefined>();
 
   useEffect(() => {
-    if (typeof nocontract === 'undefined' && ship) {
-      fetch<NoContractResponse, undefined>(`${BASE_URL}/my/ships/${ship?.symbol}/negotiate/contract`).then(res => {
-        if (res?.status === 400 || res?.status === 404) {
-          console.error('Bad Request');
-          return;
-        }
-        setNoContract(res?.data || ({} as ContractData));
-      });
-    }
-  }, [nocontract, fetch, ship]);
-
-  useEffect(() => {
     if (typeof contract === 'undefined') {
       fetch<ContractResponse, undefined>(`${BASE_URL}/my/contracts`).then(res => {
         setContract(res?.data || []);
+        if (typeof nocontract === 'undefined' && ship && !res?.data) {
+          fetch<NoContractResponse, {}>(`${BASE_URL}/my/ships/${ship?.symbol}/negotiate/contract`, {}).then(res => {
+            if (res?.status === 400 || res?.status === 404) {
+              console.error('Bad Request');
+              return;
+            }
+            setNoContract(res?.data || ({} as ContractData));
+          });
+        }
       });
     }
-  }, [contract, fetch]);
+  }, [contract, fetch, nocontract, ship]);
 
   const Accepting = (contractId: string) => {
-    fetch(`${BASE_URL}/my/contracts/${ship?.symbol}/${contractId}/accept`, {
-      method: 'POST',
+    fetch(`${BASE_URL}/my/contracts/${contractId}/accept`, {}).then(res => {
+      fetch<ContractResponse, undefined>(`${BASE_URL}/my/contracts`).then(res => {
+        setContract(res?.data || []);
+      });
     });
   };
 
@@ -55,11 +58,11 @@ export default function Home() {
                   <Paragraph>{`Faction: ${element.factionSymbol}`}</Paragraph>
                   <Paragraph>{`Type: ${element.type}`}</Paragraph>
                   <Paragraph>{`Deadline: ${element.terms.deadline}`}</Paragraph>
-                  {element.accepted === false ? (
-                  <Button key={element.id} onClick={() => Accepting(element.id)}>Accept</Button>
-                ) : (
-                  null
-                )}
+                  {!element.accepted && (
+                    <Button key={element.id} onClick={() => Accepting(element.id)}>
+                      Accept
+                    </Button>
+                  )}
                   {element.terms.deliver.map((delivery, j) => (
                     <React.Fragment key={j}>
                       <Paragraph>{`Deliver: ${delivery.tradeSymbol}`}</Paragraph>
@@ -74,16 +77,21 @@ export default function Home() {
             <p>{'No active contracts :<'}</p>
           )}
           {/* NO CONTRACT */}
-          {nocontract && (
+          {Boolean(nocontract && Object.keys(nocontract).length) && (
             <div>
               <h3 className={'mb-2 text-xl'}>Available contract</h3>
               <div className={'grid grid-cols-4'}>
-                <Paragraph>{`Faction: ${nocontract.factionSymbol}`}</Paragraph>
-                <Paragraph>{`Type: ${nocontract.type}`}</Paragraph>
-                {nocontract.terms && (
+                <Paragraph>{`Faction: ${nocontract!.factionSymbol}`}</Paragraph>
+                <Paragraph>{`Type: ${nocontract!.type}`}</Paragraph>
+                {nocontract!.terms && (
                   <>
-                    <Paragraph>{`Deadline: ${nocontract.terms.deadline}`}</Paragraph>
-                    {nocontract.terms.deliver.map((delivery, j) => (
+                    <Paragraph>{`Deadline: ${nocontract!.terms.deadline}`}</Paragraph>
+                    {nocontract?.accepted && (
+                      <Button key={nocontract.id} onClick={() => Accepting(nocontract.id)}>
+                        Accept
+                      </Button>
+                    )}
+                    {nocontract!.terms.deliver.map((delivery, j) => (
                       <React.Fragment key={j}>
                         <Paragraph>{`Deliver: ${delivery.tradeSymbol}`}</Paragraph>
                         <Paragraph>{`To: ${delivery.destinationSymbol}`}</Paragraph>
