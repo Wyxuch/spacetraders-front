fcd4cba5ff5e504cea1410fbfa52ed74fa89e297
jarek.kowalczyk <jarek.kowalczyk@dcx.bz>
2024-04-12 11:02:25 +0200

    fix types

--- src/app/(authenticated)/dashboard/materials/page.tsx
+++ src/app/(authenticated)/dashboard/materials/page.tsx
@@ -1,22 +1,24 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { BASE_URL } from '@consts/common';
 
-import { SurveyResponse } from '@api/types/survey';
+import { SurveyData, SurveyResponse } from '@api/types/survey';
 import { useShipsContext } from '@context/ShipsContext';
+
+import Paragraph from '@components/atoms/Typography/Paragraph';
 import { Button } from '@components/shadcn/ui/button';
 import { Card, CardContent } from '@components/shadcn/ui/card';
 import { useToast } from '@components/shadcn/ui/use-toast';
-import Paragraph from '@components/atoms/Typography/Paragraph';
+
 import { useApi } from '@hooks/useApi';
 
 export default function Home() {
   const { ship } = useShipsContext();
   const fetch = useApi();
   const { toast } = useToast();
-  const [surveyResponse, setSurveyResponse,] = useState<SurveyResponse | undefined>();
+  const [surveyData, setSurveyData] = useState<SurveyData | undefined>();
 
   const createSurvey = () => {
     if (!ship) {
@@ -26,26 +28,24 @@ export default function Home() {
       });
       return;
     }
-    fetch(`${BASE_URL}/my/ships/${ship.symbol}/survey`, {
-      method: 'POST'
-    })
-    .then(res =>  setSurveyResponse(res?.data || ({} as SurveyResponse)))
-    .catch(error => {
-      console.error('Error creating survey:', error);
-      toast({
-        title: 'Error',
-        description: 'Failed to create survey. Please try again later.'
+    fetch<SurveyResponse, undefined>(`${BASE_URL}/my/ships/${ship.symbol}/survey`, undefined, 'POST')
+      .then(res => setSurveyData(res?.data || ({} as SurveyData)))
+      .catch(error => {
+        console.error('Error creating survey:', error);
+        toast({
+          title: 'Error',
+          description: 'Failed to create survey. Please try again later.'
+        });
       });
-    });
   };
-  
+
   return (
     <Card className={'w-full h-full'}>
       <CardContent>
-        {surveyResponse ? (
+        {surveyData ? (
           <>
-            {surveyResponse ? (
-              surveyResponse.data.surveys.map((surveys, index) => ( 
+            {surveyData ? (
+              surveyData.surveys.map((surveys, index) => (
                 <div key={surveys.signature}>
                   <h3 className={'mb-2 text-xl'}>Survey {index + 1}</h3>
                   <div className={'grid grid-cols-4'}>
@@ -58,7 +58,7 @@ export default function Home() {
                       <li key={deposits.symbol}>{deposits.symbol}</li>
                     ))}
                   </ul>
-                </div> 
+                </div>
               ))
             ) : (
               <Button onClick={createSurvey}>Create survey</Button>
