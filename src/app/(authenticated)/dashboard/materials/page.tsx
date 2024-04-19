605f001a8749f813a91bf3ec49413a42f0da0ad6
krzysiek <krzyslenarcik@gmail.com>
2024-04-19 16:05:37 +0200

    gggg

--- src/app/(authenticated)/dashboard/materials/page.tsx
+++ src/app/(authenticated)/dashboard/materials/page.tsx
@@ -2,13 +2,12 @@
 
 import { useState } from 'react';
 import { BASE_URL } from '@consts/common';
-import { SurveyData, SurveyResponse, Surveys } from '@api/types/survey';
+import { SurveyData, SurveyResponse, Surveys} from '@api/types/survey';
 import { useShipsContext } from '@context/ShipsContext';
 import { useApi } from '@hooks/useApi';
 import { useToast } from '@components/shadcn/ui/use-toast';
 import { Button } from '@components/shadcn/ui/button';
 import { Card, CardContent } from '@components/shadcn/ui/card';
-import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
 import Paragraph from '@components/atoms/Typography/Paragraph';
 
 export default function Home() {
@@ -18,15 +17,15 @@ export default function Home() {
   const [surveyData, setSurveyData] = useState<SurveyData | undefined>();
 
   const createSurvey = () => {
-    if (ship?.nav.status === 'DOCKED') {
+    if (ship?.nav.status !== 'IN ORBIT'  && ship?.cooldown.remainingSeconds !== 0) {
       toast({
-        title: 'orbit bitch',
-        description: 'you dick'
+        title: 'Failed to create survey. Please try again later',
+        description: 'sadge'
       });
       return;
     }
     fetch<SurveyResponse, undefined>(`${BASE_URL}/my/ships/${ship.symbol}/survey`, undefined, 'POST')
-      .then(res => setSurveyData(res?.data || ({} as SurveyData)))
+      .then(res => setSurveyData(res?.data || ({} as SurveyData))) 
       .catch(error => {
         console.error('Error creating survey:', error);
         toast({
@@ -42,23 +41,10 @@ export default function Home() {
         title: 'Ship not found',
         description: 'Select Ship'
       });
+
       return;
     }
     fetch<undefined, undefined>(`${BASE_URL}/my/ships/${ship.symbol}/orbit`, undefined, 'POST')
-      .then(res => {
-        refreshShip();
-      });
-  };
-
-  const handleDock = () => {
-    if (!ship) {
-      toast({
-        title: 'Ship not found',
-        description: 'Select Ship'
-      });
-      return;
-    }
-    fetch<undefined, undefined>(`${BASE_URL}/my/ships/${ship.symbol}/dock`, undefined, 'POST')
       .then(res => {
         refreshShip();
       });
@@ -69,12 +55,13 @@ export default function Home() {
       handleOrbit();
     }
     fetch<SurveyResponse, Surveys>(`${BASE_URL}/my/ships/${ship.symbol}/extract/survey`, {
-        signature: surveyData?.surveys[0].signature,
-        symbol: surveyData?.surveys[0].symbol,
-        expiration: surveyData?.surveys[0].expiration,
-        deposits: [surveyData?.surveys[0].deposits[depositIndex]],
-        size: surveyData?.surveys[0].size,
+        signature: surveyData?.surveys[0]?.signature || "",
+        symbol: surveyData?.surveys[0]?.symbol || "",
+        expiration: surveyData?.surveys[0]?.expiration || "",
+        deposits: surveyData?.surveys[0]?.deposits || [],
+        size: surveyData?.surveys[0]?.size || "",
     }).then(() => {
+      refreshShip();
     });
   };
 
@@ -84,32 +71,27 @@ export default function Home() {
         {surveyData !== undefined ? (
           surveyData.surveys.map((survey, surveyIndex) => (
             <div key={survey.signature}>
-              <h3 className={'mb-2 text-xl'}>Survey {surveyIndex + 1}</h3>
+
+              <h3 className={'mb-2 text-xl'}>Survey {survey.size}</h3>
               <div className={'grid grid-cols-4'}>
                 <Paragraph>{`Symbol: ${survey.signature}`}</Paragraph>
+                <Paragraph>{`Expiration: ${survey.expiration}`}</Paragraph>
                 <Paragraph>{`Size: ${survey.size}`}</Paragraph>
+                <Paragraph>{`Deposits: ${survey.deposits.length}`}</Paragraph>
               </div>
               <h4 className={'mt-4 mb-2 text-lg'}>Deposits</h4>
               <ul>
                 {survey.deposits.map((deposit, depositIndex) => (
                   <li key={deposit.symbol}>
-                    {deposit.symbol}
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <span>
-                          <Button variant="link">{`Extract resource ${surveyIndex + 1}.${depositIndex + 1}`}</Button>
-                        </span>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-80">
-                        <Button onClick={() => handleExtract(survey, depositIndex)} className={'w-full'}>
-                          Extract
-                        </Button>
-                      </PopoverContent>
-                    </Popover>
+    {deposit.symbol}
                   </li>
                 ))}
               </ul>
+              <Button onClick={() => handleExtract(survey, surveyIndex)} className={'w-full'}>
+                          Extract
+                        </Button>
             </div>
+            
           ))
         ) : (
           <Button onClick={createSurvey}>Create survey</Button>
